67e53d9d689b72fe11ebc7a59a443f0bd8e18f79
MuhNaufalArhabi <naufalarhabi.mn@gmail.com>
2024-06-16 23:12:03 +0700

    fix: vercel config 5

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ app.use(cors({origin: '*'}))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.send('hai vercel')
+})
+
 app.use(route)
 app.use(err)
 
